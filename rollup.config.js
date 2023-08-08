63d4bbddb9325020440a709b7820619127d57a97
Danilo <danilomelorodrigues@gmail.com>
2023-08-08 12:22:20 -0300

    Configura o projeto e instala dependências

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import postcss from "rollup-plugin-postcss";
+import { terser } from 'rollup-plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [{
+  input: [
+    './src/index.ts',
+  ],
+  output: {
+    dir: 'dist',
+    format: 'esm',
+    preserveModules: true,
+    preserveModulesRoot: 'src',
+  },
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    postcss(),
+    commonjs(),
+    typescript(),
+    terser(),
+  ],
+  external: ['react', 'react-dom'],
+},
+
+];
