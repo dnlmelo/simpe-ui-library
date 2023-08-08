ed066dd59280ba6b91b94fe1fd56d82a7bd35d2d
Danilo <danilomelorodrigues@gmail.com>
2023-08-08 20:46:16 -0300

    exporta mixins do design system

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,10 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import postcss from "rollup-plugin-postcss";
+import copy from 'rollup-plugin-copy'
 import { terser } from 'rollup-plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default [{
   input: [
@@ -25,5 +27,14 @@ export default [{
   ],
   external: ['react', 'react-dom'],
 },
-
+{
+  input: './src/foundation/index.ts',
+  plugins: [
+    copy({
+      targets: [
+        { src: './src/foundation', dest: 'dist/' }
+      ]
+    })
+  ]
+}
 ];
