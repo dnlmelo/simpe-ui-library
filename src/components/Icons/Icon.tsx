aa527c8aff1f8b6d8bd78d6c0625abe0a8762f87
Danilo <danilomelorodrigues@gmail.com>
2023-08-24 18:18:34 -0300

    refactor: Add estilos para o ícone

--- src/components/Icons/Icon.tsx
+++ src/components/Icons/Icon.tsx
@@ -2,9 +2,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { IconName, library } from "@fortawesome/fontawesome-svg-core"
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { IconProps } from './Icon.types';
+import './icon.scss'
 
 library.add(fas);
 
 export function Icon(props:IconProps) {
-    return <FontAwesomeIcon className='icon' icon={ props.name as IconName} />
+    return(
+    <span 
+        className={ `icon icon-size-${props.size} icon-color-${props.color}` }
+    >
+        <FontAwesomeIcon 
+        icon={ props.name as IconName} />
+    </span>)
 }
--- src/components/Icons/Icon.types.ts
+++ src/components/Icons/Icon.types.ts
@@ -2,6 +2,9 @@ export interface IconProps{
     /** Nome do icon no FontAwesome */
     name: string
 
+    /** cor de fundo para o ícone */
+    color?: 'info'|'error'|'success'
+
     /** Tamanho do ícone */
     size: 'sm'| 'md'| 'lg'
 }
