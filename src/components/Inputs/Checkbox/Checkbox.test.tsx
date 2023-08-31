9f2497971145231b48a416277e6d774d791bba45
Danilo <danilomelorodrigues@gmail.com>
2023-08-31 17:24:00 -0300

    padroniza e coloca o checkbox na pasta de Inputs

---
+++ src/components/Inputs/Checkbox/Checkbox.test.tsx
@@ -0,0 +1,43 @@
+/* eslint-disable testing-library/no-node-access */
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Checkbox from './Checkbox';
+import { CheckboxProps } from './Checkbox.types';
+
+describe('<Checkbox />', () => {
+  test('it should mount', () => {
+    render(<Checkbox size={'lg'} />);
+    const inputRadio = screen.getByTestId('Checkbox');
+    expect(inputRadio).toBeInTheDocument();
+  });
+
+  describe('check attr size', () => {
+    const sizes:CheckboxProps['size'][] = [
+      'lg',
+      'md'
+    ]
+
+    sizes.forEach(s =>{
+      test(`check size ${s}`, ()=>{
+        render(<Checkbox size={s} id='x' />);
+        const input = screen.getByTestId('Checkbox');
+        expect(input.classList.contains(`checkbox-size-${s}`)).toBeTruthy()
+      })
+    })
+  });
+
+
+  test('check attr errorMessage', () => {
+    render(<Checkbox size='md' errorMessage='error' error={true} />);
+    const msg = screen.getByTestId('errorMsg');
+
+    expect(msg.textContent).toEqual('error')
+  });
+
+  test('check attr infoMessage', () => {
+    render(<Checkbox size='md' infoMessage='info' />);
+    const input = screen.getByTestId('Checkbox');
+
+    expect(input.querySelector('.icon')).toBeInTheDocument()
+  });
+});
