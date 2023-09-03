7ee34cdb92b48971ceac2b4bf0f35b051488080e
Danilo <danilomelorodrigues@gmail.com>
2023-09-03 19:46:02 -0300

    feat: Component Stepper

---
+++ src/components/Stepper/Stepper.test.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable testing-library/no-node-access */
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Stepper from './Stepper';
+
+describe('<Stepper />', () => {
+  const _steps = [
+    'Step 1',
+    'Step 2'
+  ];
+  test('it should mount', () => {
+    render(<Stepper steps={_steps} completedSteps={'allBeforeActive'} />);
+    const steps = screen.getByTestId('Steps');
+    expect(steps).toBeInTheDocument();
+  });
+
+  test('check activedStep attr', ()=>{
+    render(<Stepper steps={_steps} completedSteps={'allBeforeActive'} activedStep={1} />);
+    const stepper = screen.getByTestId('Steps')
+    const actived = stepper.querySelector('[data-actived=true]')
+    expect(actived.textContent).toContain('Step 2')
+  })
+
+  test('check completeSteps attr with "allBeforeActive" value', ()=>{
+    render(<Stepper steps={_steps} completedSteps={'allBeforeActive'} activedStep={1} />);
+    const stepper = screen.getByTestId('Steps')
+    const actived = stepper.querySelector('[data-actived=true]')
+    const completed = stepper.querySelector('[data-completed=true]')
+    expect(actived.textContent).toContain('Step 2')
+    expect(completed.textContent).toContain('Step 1')
+  })
+
+  test('check completeSteps attr with "all" value', ()=>{
+    render(<Stepper steps={_steps} completedSteps={'all'} activedStep={1} />);
+    const stepper = screen.getByTestId('Steps')
+    const completed = stepper.querySelectorAll('[data-completed=true]')
+    expect(completed.length).toBe(_steps.length)
+  })
+});
---
+++ src/components/Stepper/Stepper.tsx
@@ -0,0 +1,50 @@
+import { FC } from 'react';
+import './Stepper.scss';
+
+interface StepsProps {
+  /** Index do Step ativo. Quando indefinido, o primeiro será ativado ao renderizar */
+  activedStep?: number;
+
+  /** Índices de steps completados ao iniciar.
+   * Aceita um array de indíces ou a flag 'allBeforeActive. 
+   * Se a Flag for passada. Os passos anteriores estarão com o estado de "completos"
+   * */
+  completedSteps: 'allBeforeActive' | 'all';
+
+  /** Array com os labels do Step. Cada item será um step  */
+  steps:string[];
+}
+
+const Stepper: FC<StepsProps> = (props) => {
+  const { activedStep = 0, completedSteps} = props;
+
+  function setActived(i:number) {
+    return activedStep === i
+  }
+
+  function setCompleted(i:number){
+    if(completedSteps === 'all') return true;
+
+    if(completedSteps === 'allBeforeActive'){
+      return i < activedStep
+    }
+  }
+
+  return (
+  <div className='steps' data-testid="Steps">
+    {props.steps.map((s, i)=>
+      <div 
+        key={i} 
+        className='step' 
+        data-actived={setActived(i)}
+        data-completed={setCompleted(i)}
+        data-name={`step-${i}`} 
+      >
+        <span className='step-count'>{i+1}</span>
+        <span className='step-label'>{s}</span>
+      </div>
+    )}
+  </div>)
+
+}
+export default Stepper;
