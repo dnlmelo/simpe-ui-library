ac95394118fa2a55641e2475a394ff9225e3d6cc
Danilo <danilomelorodrigues@gmail.com>
2023-09-13 16:21:15 -0300

    feat: plugin pra tratar exportar img

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
-import copy from "rollup-plugin-copy";
 import { terser } from "rollup-plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import image from '@rollup/plugin-image';
