a0e3a194a964af396e620805d32546b8aada95cf
Danilo Rodrigues <danilomelorodrigues@gmail.com>
2023-09-29 17:07:12 -0300

    fix: onChange do Input Currency não respondia os valores raw e masked

--- src/components/Inputs/InputCurrency/InputCurrency.types.ts
+++ src/components/Inputs/InputCurrency/InputCurrency.types.ts
@@ -1,10 +1,11 @@
 import { InputProps } from "../Input-base.types";
 
-export interface InputCurrencyProps extends InputProps{
+export interface InputCurrencyProps extends Omit<InputProps, 'onChange'>{
   /** Número máximo Permitido. Pode ser um Inteiro ou Float */
   max: number
 
   /** Valor inicial do input, se não for passado, inciará com 0 (R$0,00)*/
   value?:number
+  onChange?(event, value, maskedValue): any
 }
   
