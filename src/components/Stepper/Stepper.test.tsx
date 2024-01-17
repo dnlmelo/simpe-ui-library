6823f9d2b7a50e02a28adc0bc7b350d7c7cc70d5
Douglas Franco <douglas.franco@finvity.com.br>
2024-01-17 13:49:33 -0300

    add the ability to return the index of the clicked step

--- src/components/Stepper/Stepper.test.tsx
+++ src/components/Stepper/Stepper.test.tsx
@@ -1,39 +1,64 @@
 /* eslint-disable testing-library/no-node-access */
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import {Stepper} from './Stepper';
+import { fireEvent, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { Stepper } from "./Stepper";
 
-describe('<Stepper />', () => {
-  const _steps = [
-    'Step 1',
-    'Step 2'
-  ];
-  test('it should mount', () => {
-    render(<Stepper steps={_steps} completedSteps={'allBeforeActive'} />);
-    const steps = screen.getByTestId('Steps');
+describe("<Stepper />", () => {
+  const _steps = ["Step 1", "Step 2"];
+  test("it should mount", () => {
+    render(<Stepper steps={_steps} completedSteps={"allBeforeActive"} />);
+    const steps = screen.getByTestId("Steps");
     expect(steps).toBeInTheDocument();
   });
 
-  test('check activedStep attr', ()=>{
-    render(<Stepper steps={_steps} completedSteps={'allBeforeActive'} activedStep={1} />);
-    const stepper = screen.getByTestId('Steps')
-    const actived = stepper.querySelector('[data-actived=true]')
-    expect(actived.textContent).toContain('Step 2')
-  })
+  test("check activedStep attr", () => {
+    render(
+      <Stepper
+        steps={_steps}
+        completedSteps={"allBeforeActive"}
+        activedStep={1}
+      />
+    );
+    const stepper = screen.getByTestId("Steps");
+    const actived = stepper.querySelector("[data-actived=true]");
+    expect(actived.textContent).toContain("Step 2");
+  });
 
-  test('check completeSteps attr with "allBeforeActive" value', ()=>{
-    render(<Stepper steps={_steps} completedSteps={'allBeforeActive'} activedStep={1} />);
-    const stepper = screen.getByTestId('Steps')
-    const actived = stepper.querySelector('[data-actived=true]')
-    const completed = stepper.querySelector('[data-completed=true]')
-    expect(actived.textContent).toContain('Step 2')
-    expect(completed.textContent).toContain('Step 1')
-  })
+  test('check completeSteps attr with "allBeforeActive" value', () => {
+    render(
+      <Stepper
+        steps={_steps}
+        completedSteps={"allBeforeActive"}
+        activedStep={1}
+      />
+    );
+    const stepper = screen.getByTestId("Steps");
+    const actived = stepper.querySelector("[data-actived=true]");
+    const completed = stepper.querySelector("[data-completed=true]");
+    expect(actived.textContent).toContain("Step 2");
+    expect(completed.textContent).toContain("Step 1");
+  });
 
-  test('check completeSteps attr with "all" value', ()=>{
-    render(<Stepper steps={_steps} completedSteps={'all'} activedStep={1} />);
-    const stepper = screen.getByTestId('Steps')
-    const completed = stepper.querySelectorAll('[data-completed=true]')
-    expect(completed.length).toBe(_steps.length)
-  })
+  test('check completeSteps attr with "all" value', () => {
+    render(<Stepper steps={_steps} completedSteps={"all"} activedStep={1} />);
+    const stepper = screen.getByTestId("Steps");
+    const completed = stepper.querySelectorAll("[data-completed=true]");
+    expect(completed.length).toBe(_steps.length);
+  });
+
+  test("check onClickStep attr", () => {
+    const onClickStep = jest.fn();
+    render(
+      <Stepper
+        steps={_steps}
+        completedSteps={"allBeforeActive"}
+        activedStep={1}
+        onClickStep={onClickStep}
+      />
+    );
+    const stepper = screen.getByTestId("Steps");
+    const step = stepper.querySelector('[data-name="step-0"]');
+    fireEvent.click(step);
+    expect(onClickStep).toHaveBeenCalled();
+  });
 });
